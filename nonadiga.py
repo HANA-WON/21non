c42568b0df3183caf9de8239ef2bc4f1c718f006
HANA-WON <HANA-WON@users.noreply.github.com>
2020-08-24 21:14:27 +0900

    playlord 편집
    필요없는 부분 편집완료

--- nonadiga.py
+++ nonadiga.py
@@ -3,7 +3,7 @@ import requests
 
 url = "http://adiga.kr/kcue/ast/eip/eis/inf/selctninf/EipSelctnInfGnrlList.do?p_menu_id=undefined"
 
-payload = 'bfyr_at=&btn_list_sort=&chk_area=&chk_rcrr=20&chk_search_area=all&chk_selctnNm=&chk_slcty=04&cho_area=&cho_univ=&cur_year=2021&dgnss_at=Y&dtl_rcbkSerch=N&dtl_serch=N&gnrlKeyword=&lm_cd=&lst_area_cd=&lst_lgcl_cd=&lst_lgcl_grp=&lst_mdcl_cd=&lst_mdcl_grp=&lst_rcrr_cd=&lst_selctn_nm_cd=&lst_slcty_cd=&lst_smcl_cd=&lst_smcl_grp=&lst_subjct_grp=&main_search_at=&pageIndex=2&pageSize=15&rcrr=20&rcu_cd=&sch_year=2021&searchKeyword=&searchUnivKeyword=&sel_area=&sel_csat_imtexm=&sel_rcrr=20&sel_selctn_nm=&sel_sers=&sel_slcty=04&send_csat_imtexm=&send_rcrr_cd=&send_rcu_cd=&send_rcu_sn=&send_sch_year=&send_searchKeyword=&send_selctn_grp_cd=&send_sers_cd=&send_univ_cd=&send_univ_nm=&sers=&stdr_sch_year=&subjct_keyword=&this_sch_year=2021&univ_cd=&univ_keyword=&univ_se_cd=10&univ_sort=UP&uv_type=on'
+payload = 'chk_rcrr=20&chk_slcty=04&cur_year=2021&dgnss_at=Y&dtl_rcbkSerch=N&dtl_serch=N&lst_lgcl_cd=&lst_mdcl_cd=&lst_smcl_cd=&pageIndex=1&pageSize=15&sch_year=2021&sel_area=&sel_rcrr=20&sel_selctn_nm=&sel_slcty=04&sers=&this_sch_year=2021&univ_sort=UP&uv_type=on'
 headers = {
   'Accept': 'application/json, text/javascript, */*; q=0.01',
   'X-Requested-With': 'XMLHttpRequest',
