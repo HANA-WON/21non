63e14252dac05513e9a4f18c7d50f6d057bd936b
HANA-WON <HANA-WON@users.noreply.github.com>
2020-08-31 22:10:33 +0900

    쿼리스트링, args
    ajax 이용해서 쿼리 스트링을 만들고... flask 서버에서는 request.args 이용해서 가져올 수 있다.

--- app.py
+++ app.py
@@ -5,10 +5,18 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+@app.route('/')
+def searchsub():
+    query_receive = request.args['subject']
+    list(db.nonsul.find({'subject': {'$regex': query_receive}}))
+
+    
 @app.route('/nonsul')
 def screening():
     screenings = list(db.nonsul.find({}, {'_id': 0}))
